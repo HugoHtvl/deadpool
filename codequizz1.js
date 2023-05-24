0f1f1af91574e1d9b26f009793d4d5a2467dea05
SebastienANTICOURT <sebastien.anticourt@live.fr>
2023-05-24 14:20:36 +0200

    code1

--- codequizz1.js
+++ codequizz1.js
@@ -1,5 +1,34 @@
 
-const buttont = document.querySelectorAll(".buttont");
+function toggleMenu() {
+    const navbar = document.querySelector('.navbar');
+    const menuOverlay = document.querySelector('menuOverlay');
+
+    if (navbar.classList.contains("active")) {
+        navbar.classList.remove('active');
+        menuOverlay.classList.remove('active')
+    } else {
+        navbar.classList.add('active');
+        menuOverlay.classList.add('active');
+    }
+}
+
+document.querySelector(".winbutton").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "block";
+  });
+  
+  document.getElementById("goToPage").addEventListener("click", function() {
+    window.location.href = "another_page.html";
+  });
+
+/*document.getElementById("winbutton").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "block";
+  });
+  
+  document.getElementById("goToPage").addEventListener("click", function() {
+    window.location.href = "another_page.html";
+  });
+
+/*const buttont = document.querySelectorAll(".buttont");
 for (let i=0; i<buttont.length; i++) {
 buttont[i].addEventListener("click", function() {
 buttont[i].style.color = "black";
@@ -10,5 +39,5 @@ buttont[i].style.backgroundColor = "red";
 const victoire = document.querySelector("#victoire");
 victoire.addEventListener("click", function() {
 victoire.
-b});
+b});*/
 
