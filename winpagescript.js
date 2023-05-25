61490ff1648974d4fa0d8d95d36b5f9249e6a443
NayanN91 <nayan.nanouche@gmail.com>
2023-05-25 10:11:21 +0200

    modified:   burgerstyles/winpage.css modified:   burgerwinpage.html modified:   burgerwinpagescript.js

--- winpagescript.js
+++ winpagescript.js
@@ -1,5 +1,4 @@
-//Menu
-
+// JS de Nayan nav burger
 function toggleMenu() {
     const navbar = document.querySelector('.navbar');
     const menuOverlay = document.querySelector('menuOverlay');
@@ -11,6 +10,25 @@ function toggleMenu() {
         navbar.classList.add('active');
         menuOverlay.classList.add('active');
     }
-    
-
 }
+// JS Hugo burger
+const headerOn = document.querySelector(".headerMenu");
+const headerOff = document.querySelector(".navbarClose");
+
+headerOn.addEventListener("click", () => {
+    if (getComputedStyle(headerOn).display != "none") {
+        headerOn.style.display = "none";
+    } else {
+        headerOn.style.display = "block";
+    }
+});
+
+headerOff.addEventListener("click", () => {
+    if (getComputedStyle(headerOn).display === "none") {
+        setTimeout(() => {
+            headerOn.style.display = "block";
+        }, 300);
+    } else {
+        headerOn.style.display = "none";
+    }
+});
